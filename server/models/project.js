502d05262690209f9d1d5b8541baffb820e23c29
Reverse117 <cecytno9@gmail.com>
2018-10-28 18:13:54 -0600

    Reset the original project model

--- server/models/project.js
+++ server/models/project.js
@@ -3,14 +3,14 @@ const { Schema } = mongoose;
 
 const ProjectSchema = new Schema({
     name: { type: String, required: true },
-    description: { type: String, default: '', required: false },
-    storeName: {type: String, default: '', required: false },
-    storeNumber: { type: Number, default: 0 , required: false },
-    m2: { type: Number, default: 0, required: false },
-    location: { type: String, default: '', required: false },
-    localReception: { type: Date, default: null, required: false },
-    openingDate: { type: Date, default: null, required: false },
-    furnitureDate: { type: Date, default: null, required: false },
+    description: { type: String, required: false },
+    storeName: {type: String, required: false },
+    storeNumber: { type: Number, required: false },
+    m2: { type: Number, required: false },
+    location: { type: String, required: false },
+    localReception: { type: Date, required: false },
+    openingDate: { type: Date, required: false },
+    furnitureDate: { type: Date, required: false },
     alertsActivated: {type: Boolean, default: true, required: true},
     activities: [{ type: Schema.Types.ObjectId, ref: "Activity", default: [], required: true }]
 });
