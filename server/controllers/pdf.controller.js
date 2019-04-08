ee9aa4680c504c28e1a75bf1636a9e97aa39cad7
Reverse117 <cecytno9@gmail.com>
2019-04-08 10:21:47 -0500

    Document + image dimensions

--- server/controllers/pdf.controller.js
+++ server/controllers/pdf.controller.js
@@ -10,141 +10,130 @@ moment().format();
 moment.locale('es');
 
 pdfCtrl.createPDF = async (req, res) => {
-  const doc = new PDFDocument({
-    info: {
-      Title: 'Informe',
-      Author: 'Virtual Assistant'
-    }
-  });
-  sizeOf('uploads/'+req.file.filename, function (err, dimensions) {
-    console.log(dimensions.width, dimensions.height);
-  });
-  res.json({});
-  /*doc.image('uploads/'+req.file.filename);
-  res.writeHead(200, {
-    'Content-Type': 'application/pdf',
-    'Access-Control-Allow-Origin': '*',
-    'Content-Disposition': 'attachment; filename=Informe.pdf'
-  });
-  doc.pipe(res);
-  doc.end();*/
-  /*
   const { id } = req.params;
   var now = moment().format('L');
+  const URI = 'uploads/'+req.file.filename;
   await Project.findById(id)
   .then(async (project) => {
     const resident = await User.findOne({projects: project._id, userType: 2});
     const designer = await User.findOne({projects: project._id, userType: 3});
-    console.log(resident);
-    const doc = new PDFDocument({
-      info: {
-        Title: 'Informe',
-        Author: 'Virtual Assistant'
+    sizeOf(URI, function (err, dimensions) {
+      const doc = new PDFDocument({
+        info: {
+          Title: 'Informe',
+          Author: 'Virtual Assistant'
+        }
+      });
+      doc.image('src/Logo.jpg',156,60, {
+        width: 300
+      });
+      doc.moveDown(4);
+      doc.font('Times-Roman').fontSize(20).text('   INFORME DE CIERRE DE OBRA   ', {
+        align: 'center',
+        underline: true
+      });
+      doc.moveDown(1);
+      doc.font('Helvetica-Bold').fontSize(18).text(project.name, {
+        align: 'center'
+      });
+      doc.font('Helvetica').fontSize(15).text(project.description, {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Nombre de la Tienda: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(project.storeName, {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Número de la Tienda: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(project.storeNumber, {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Metraje: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(project.m2, {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Ubicación: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(project.location, {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Residente a Cargo: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(resident.name, {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Proyectista a Cargo: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(designer.name, {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Fecha de Recepción del Local: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(moment(project.localReception).format('LL'), {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Fecha de Pedido de Muebles: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(moment(project.furnitureDate).format('LL'), {
+        align: 'justify'
+      });
+      doc.moveDown();
+      doc.font('Helvetica-Bold').fontSize(15).text('Fecha de Apertura del Local: ', {
+        align: 'justify',
+        continued: true
+      });
+      doc.font('Helvetica').fontSize(15).text(moment(project.openingDate).format('LL'), {
+        align: 'justify'
+      });
+      doc.font('Courier-Oblique').fontSize(9).text('Generado el ' + now, 248, 710);
+      doc.addPage();
+      if (dimensions.width >= doc.page.width) {
+        doc.image(URI, 15,60, {
+          width: (doc.page.width-30)
+        });
+      } else {
+        doc.image(URI, (doc.page.width - dimensions.width)/2);
       }
+      res.writeHead(200, {
+        'Content-Type': 'application/pdf',
+        'Access-Control-Allow-Origin': '*',
+        'Content-Disposition': 'attachment; filename=Informe.pdf'
+      });
+      doc.pipe(res);
+      doc.end();
     });
-    doc.image('src/Logo.jpg',156,60, {
-      width: 300
-    });
-    doc.moveDown(4);
-    doc.font('Times-Roman').fontSize(20).text('   INFORME DE CIERRE DE OBRA   ', {
-      align: 'center',
-      underline: true
-    });
-    doc.moveDown(1);
-    doc.font('Helvetica-Bold').fontSize(18).text(project.name, {
-      align: 'center'
-    });
-    doc.font('Helvetica').fontSize(15).text(project.description, {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Nombre de la Tienda: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(project.storeName, {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Número de la Tienda: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(project.storeNumber, {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Metraje: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(project.m2, {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Ubicación: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(project.location, {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Residente a Cargo: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(resident.name, {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Proyectista a Cargo: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(designer.name, {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Fecha de Recepción del Local: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(moment(project.localReception).format('LL'), {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Fecha de Pedido de Muebles: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(moment(project.furnitureDate).format('LL'), {
-      align: 'justify'
-    });
-    doc.moveDown();
-    doc.font('Helvetica-Bold').fontSize(15).text('Fecha de Apertura del Local: ', {
-      align: 'justify',
-      continued: true
-    });
-    doc.font('Helvetica').fontSize(15).text(moment(project.openingDate).format('LL'), {
-      align: 'justify'
-    });
-    doc.font('Courier-Oblique').fontSize(9).text('Generado el ' + now, 248, 710);
-    doc.addPage();
-    res.writeHead(200, {
-      'Content-Type': 'application/pdf',
-      'Access-Control-Allow-Origin': '*',
-      'Content-Disposition': 'attachment; filename=Informe.pdf'
-    });
-    doc.pipe(res);
-    doc.end();
   })
   .catch(() => {
     res.json({
       status: 'No File'
     });
   });
-  */
   };//Almost Finished
 
 module.exports = pdfCtrl;
