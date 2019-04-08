28e3bb4b6a779c58619751d514809f5fbb9182e6
Reverse117 <cecytno9@gmail.com>
2019-04-07 21:17:01 -0500

    Change to POST Android routes

--- server/routes/android.routes.js
+++ server/routes/android.routes.js
@@ -5,6 +5,6 @@ const user = require('../controllers/user.controller');
 const alert = require('../controllers/alert.controller');
 
 router.post('/login', user.loginAndroid);//Checked
-router.get('/alerts', alert.getProjectAlerts);//Checked
+router.post('/alerts', alert.getProjectAlerts);//Checked
 
 module.exports = router;
