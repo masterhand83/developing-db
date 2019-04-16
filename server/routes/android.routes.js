7ec34e76cb681b0991bd6bc0de8033c4360ba15f
Reverse117 <cecytno9@gmail.com>
2019-04-16 08:51:14 -0500

    New Android Routes

--- server/routes/android.routes.js
+++ server/routes/android.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const user = require('../controllers/user.controller');
 const alert = require('../controllers/alert.controller');
+const activity = require('../controllers/activity.controller');
 
 router.post('/login', user.loginAndroid);//Checked
 router.post('/alerts', alert.getProjectAlerts);//Checked
