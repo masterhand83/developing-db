196f0115f9b3d9afdf05ee8d84c5794c41ad1395
Reverse117 <cecytno9@gmail.com>
2019-04-16 08:48:12 -0500

    New Android Methods

--- server/routes/android.routes.js
+++ server/routes/android.routes.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const user = require('../controllers/user.controller');
 const alert = require('../controllers/alert.controller');
+const activity = require('../controllers/activity.controller');
 
 router.post('/login', user.loginAndroid);//Checked
 router.post('/alerts', alert.getProjectAlerts);//Checked
+router.get('/comment/:id', activity.getCommentsAndroid);//Checked
+router.put('/priority/:id', activity.changePriorityAndroid);//Checked
 
 module.exports = router;
